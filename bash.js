d826ed090fa0ad7a5c47deed6d1a0dbcc0ab5e6a
Ian Dewsbury <iandewsbury@Ians-MacBook-Pro.local>
2021-07-20 14:01:44 -0400

    implement pwd and date

--- bash.js
+++ bash.js
@@ -4,7 +4,12 @@ process.stdout.write('prompt > ')
 // stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
     const cmd = data.toString().trim() // remove newline
-
-    process.stdout.write('You typed: ' + cmd)
+    if ( cmd == 'pwd') process.stdout.write(process.cwd())
+    else if ( cmd == 'date' ) {
+        const date = new Date()
+        process.stdout.write(date.toLocaleDateString())
+    }
+    else process.stdout.write('You typed: ' + cmd)
+    
     process.stdout.write('\nprompt > ')
 })
