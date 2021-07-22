a73add78b4b58a1f1afa2ad97d4f708bf1a6f0e5
Ian Dewsbury <iandewsbury@Ians-MacBook-Pro.local>
2021-07-22 14:59:14 -0400

    add fs command

--- bash.js
+++ bash.js
@@ -6,8 +6,7 @@ process.stdout.write('prompt > ')
 // stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
     const cmd = data.toString().trim() // remove newline
-    if ( cmd == 'pwd') commands.pwd()
-    else if ( cmd == 'date' ) commands.date()
+    if (commands.hasOwnProperty(cmd)) commands[cmd]()
     else process.stdout.write('You typed: \'' + cmd +'\'. Command not found.')
     
     process.stdout.write('\nprompt > ')
--- commands.js
+++ commands.js
@@ -1,8 +1,17 @@
+const fs = require('fs')
+
 module.exports = {
     pwd: () => process.stdout.write(process.cwd()),
     date: () => {
         const date = new Date()
         process.stdout.write(date.toLocaleDateString())
-
+    },
+    ls: () => {
+        fs.readdir('.', (err, files) => {
+            if (err) throw err;
+            files.forEach((file) => {
+                process.stdout.write(file.toString() + '\n')
+            })
+        })
     }
 }
